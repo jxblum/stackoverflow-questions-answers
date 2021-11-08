4a744426ad4f576887c56a07b4f0edcc72cb17fb
John Blum <jblum@vmware.com>
2021-11-07 19:06:10 -0800

    Create the CacheManagerDecoratingBeanPostProcessor to post-process an existing CacheManager implementation from a caching provider for the purpose of decorating the CacheManager implementaton behavior with a pluggable Decorator implementation.

---
+++ spring-cache/src/main/java/io/stackoverflow/questions/answers/spring/cache/CacheManagerDecoratingBeanPostProcessor.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2020-present the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+package io.stackoverflow.questions.answers.spring.cache;
+
+import java.util.function.Function;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.cache.CacheManager;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+
+/**
+ * Spring {@link BeanPostProcessor} that {@literal decorates} a Spring {@link CacheManager} bean
+ * registered in the Sprig container for the Spring application.
+ *
+ * @author John Blum
+ * @see java.util.function.Function
+ * @see org.springframework.beans.factory.config.BeanPostProcessor
+ * @see org.springframework.cache.CacheManager
+ * @see <a href="https://en.wikipedia.org/wiki/Decorator_pattern">Decorator Software Design Pattern</a>
+ * @since 1.0.0
+ */
+public class CacheManagerDecoratingBeanPostProcessor implements BeanPostProcessor {
+
+	/**
+	 * Factory method used to construct a new instance of {@link CacheManagerDecoratingBeanPostProcessor}
+	 * initialized with the given {@link Function} used to {@literal decorate} the {@link CacheManager} bean
+	 * registered in the Spring context.
+	 *
+	 * @param cacheManagerDecoratorFunction {@link Function} used to {@literal decorate}
+	 * the registered {@link CacheManager} bean.
+	 * @see #CacheManagerDecoratingBeanPostProcessor(Function)
+	 * @see org.springframework.cache.CacheManager
+	 * @see java.util.function.Function
+	 */
+	public static @NonNull CacheManagerDecoratingBeanPostProcessor from(
+			@Nullable Function<CacheManager, CacheManager> cacheManagerDecoratorFunction) {
+
+		return new CacheManagerDecoratingBeanPostProcessor(cacheManagerDecoratorFunction);
+	}
+
+	@Getter(AccessLevel.PROTECTED)
+	private final Function<CacheManager, CacheManager> cacheManagerDecoratorFunction;
+
+	/**
+	 * Constructs a new instance of {@link CacheManagerDecoratingBeanPostProcessor} initialized with
+	 * the given {@link Function} used to {@literal decorate} the {@link CacheManager} bean
+	 * registered in the Spring context.
+	 *
+	 * @param cacheManagerDecoratorFunction {@link Function} used to {@literal decorate}
+	 * the registered {@link CacheManager} bean.
+	 * @see org.springframework.cache.CacheManager
+	 * @see java.util.function.Function
+	 */
+	public CacheManagerDecoratingBeanPostProcessor(
+			@Nullable Function<CacheManager, CacheManager> cacheManagerDecoratorFunction) {
+
+		this.cacheManagerDecoratorFunction = cacheManagerDecoratorFunction != null
+			? cacheManagerDecoratorFunction
+			: Function.identity();
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	@Override
+	public @NonNull Object postProcessAfterInitialization(@NonNull Object bean, @NonNull String beanName)
+			throws BeansException {
+
+		return bean instanceof CacheManager
+			? getCacheManagerDecoratorFunction().apply((CacheManager) bean)
+			: bean;
+	}
+}
